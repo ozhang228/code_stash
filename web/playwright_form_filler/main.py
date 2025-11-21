5bea4d675bb4edd1a39e707b604753d797ddc2c2
Oscar Zhang <oscarzhang228@gmail.com>
2025-11-20 19:11:12 -0500

    smaller page and add email support

--- web/playwright_form_filler/main.py
+++ web/playwright_form_filler/main.py
@@ -6,7 +6,7 @@ from config import config
 def main():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
+        page = browser.new_page(viewport={"width": 900, "height": 600})
 
         page.goto(config.page_url)
 
@@ -14,25 +14,27 @@ def main():
         page.pause()
 
         for step in config.steps:
-            for selector, value in step.fields.items():
-                element = page.query_selector(selector)
+            for field in step.fields:
+                element = page.query_selector(field.selector)
 
                 if not element:
-                    print(f"Could not find element with selector {selector}")
+                    print(f"Could not find element with selector {field.selector}")
                     continue
 
                 element_type = element.evaluate("el => el.type")
                 match element_type:
                     case "select":
-                        element.select_option(value)
+                        element.select_option(field.value)
                     case "radio":
                         element.check()
                     case "checkbox":
                         element.check()
                     case "text":
-                        page.fill(selector, value)
+                        page.fill(field.selector, field.value)
                     case "textarea":
-                        page.fill(selector, value)
+                        page.fill(field.selector, field.value)
+                    case "email":
+                        page.fill(field.selector, field.value)
                     case _:
                         assert False, f"Unsupported element type {element.type}"
 
