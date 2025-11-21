5179a165f6278a973696fccad0367a432f51537c
Oscar Zhang <oscarzhang228@gmail.com>
2025-11-20 19:11:05 -0500

    make config easier to use

--- web/playwright_form_filler/config.py
+++ web/playwright_form_filler/config.py
@@ -1,11 +1,41 @@
-from pydantic import BaseModel
+from typing import Protocol
+
+from pydantic import BaseModel, computed_field
 
 FieldSelector = str
 FieldValue = str
 
 
+class Field(BaseModel, frozen=True):
+    @property
+    def selector(self) -> FieldSelector:
+        raise NotImplementedError
+
+    value: FieldValue
+
+
+class RadioField(Field, frozen=True):
+    name: str
+    value: FieldValue
+
+    @computed_field
+    @property
+    def selector(self) -> FieldSelector:
+        return f"input[name='{self.name}'][value='{self.value}']"
+
+
+class TextField(Field, frozen=True):
+    id: str
+    value: FieldValue
+
+    @computed_field
+    @property
+    def selector(self) -> FieldSelector:
+        return f"#{self.id}"
+
+
 class Step(BaseModel):
-    fields: dict[FieldSelector, FieldValue]
+    fields: list[Field]
 
 
 class Config(BaseModel):
@@ -14,18 +44,10 @@ class Config(BaseModel):
 
 
 PAGE_URL = "https://kyc.cumberland.io/onboard-entity"
+
 STEPS = [
-    Step(
-        fields={
-            "#accepted_minimum_trade>input": "",
-            "#accepted_terms_conditions>input": "",
-        }
-    ),
-    Step(
-        fields={
-            "input[name='entity_ecp_criteria'][value='Yes']": "",
-        }
-    ),
+    Step(fields=[TextField(id="accepted_minimum_trade>input", value="hi")]),
+    Step(fields=[RadioField(name="entity_ecp_criteria", value="Yes")]),
 ]
 
-config = Config(page_url=PAGE_URL, steps=[])
+config = Config(page_url=PAGE_URL, steps=STEPS)
